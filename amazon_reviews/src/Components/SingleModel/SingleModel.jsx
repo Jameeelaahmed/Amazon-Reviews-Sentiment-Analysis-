0b5d93c0f2f01ee61d154e113fc3758c791e4d31
Jameeelaahmed <gamilas320@gmail.com>
2024-05-15 04:26:50 +0300

    models

--- amazon_reviews/src/Components/SingleModel/SingleModel.jsx
+++ amazon_reviews/src/Components/SingleModel/SingleModel.jsx
@@ -22,6 +22,12 @@ export default function SingleModel() {
 
     return (
         <div className={classes.container}>
+            <div className={classes.models}>
+                    <button className={classes.button}>Naive Bayes</button>
+                    <button className={classes.button}>Rnn</button>
+                    <button className={classes.button}>Roberta</button>
+            </div>
+
             <div className={classes.subscribe}>
                 <form onSubmit={handleSubmit}>
                     <textarea placeholder="Enter your email" ref={reviewRef}></textarea>
@@ -29,13 +35,6 @@ export default function SingleModel() {
                 </form>
             </div>
 
-            <div className={classes.models}>
-                <div className={classes.tabs}>
-                    <div className={classes.model}>Naive Bayes</div>
-                    <div className={classes.model}>Rnn</div>
-                    <div className={classes.model}>Roberta</div>
-                </div>
-            </div>
 
             <div className={classes.emojies}>
                 <div id="angry" className={classes.emoji} onClick={handleFocus1}>
