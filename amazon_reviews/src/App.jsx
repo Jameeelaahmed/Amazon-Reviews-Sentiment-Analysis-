9831059a42b0c44a41e28c721ef4ae0e17b0005d
Jameeelaahmed <gamilas320@gmail.com>
2024-05-15 03:42:55 +0300

    with_react

---
+++ amazon_reviews/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SingleModel from './Components/SingleModel/SingleModel'
+
+function App() {
+
+  return (
+    <>
+    <SingleModel></SingleModel>
+    </>
+  )
+}
+
+export default App
